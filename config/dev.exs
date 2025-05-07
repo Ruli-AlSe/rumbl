7122bd3fccd1645b6507c68efc63968b419f1bc5
Raul Alejandro Almanza Serrano <ra.almanzaserrano@gmail.com>
2025-05-07 13:25:31 -0600

    Implementar changeset de Ecto para validar y castear informacion proveniente del cliente que puede afectar al programa

--- config/dev.exs
+++ config/dev.exs
@@ -25,7 +25,7 @@ config :rumbl, RumblWeb.Endpoint,
       "node_modules/webpack/bin/webpack.js",
       "--mode",
       "development",
-      "--watch",
+      # "--watch",
       # "--watch-options-stdin",
       cd: Path.expand("../assets", __DIR__)
     ]
