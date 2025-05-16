9b379b38dd4299c708d2a30f8ad4a6ec90b824d7
Raul Alejandro Almanza Serrano <ra.almanzaserrano@gmail.com>
2025-05-16 01:32:08 -0600

    Agregar una fucion en el socket de video para que escuche eventos "ping" y muestre en consola el valor de la variable accounts

--- assets/js/video.js
+++ assets/js/video.js
@@ -3,7 +3,7 @@ import Player from './player';
 let Video = {
     init(socket, element) {
         if(!element) return;
-
+        
         let playerId = element.getAttribute("data-player-id");
         let videoId = element.getAttribute("data-id");
         socket.connect();
@@ -16,6 +16,11 @@ let Video = {
         let msgInput = document.getElementById("msg-input");
         let postButton = document.getElementById("msg-submit");
         let vidChannel = socket.channel("videos:" + videoId)
+        vidChannel.on("ping", ({count}) => console.log("PING", count));
+
+        vidChannel.join()
+            .receive("ok", resp => console.log("Joined the video channel", resp))
+            .receive("error", reason => console.log("Join failed", reason));
     }
 }
 
